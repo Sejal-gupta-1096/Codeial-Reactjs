d21b70a7aa20ad3e2c5485e902aaf5abc4900ce3
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-08-08 04:23:28 -0700

    Saving form data in local state (Controlled component)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      email : '',
+      password : ''
+    };
+  }
+
+  handleEmail = (event) =>{
+    this.setState({
+      email : event.target.value
+    })
+  }
+
+  handlePassword = (event) =>{
+    this.setState({
+      password : event.target.value
+    })
+  }
+
+  handleFormSubmit = (event) =>{
+    event.preventDefault();
+    console.log(this.state);
+  }
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input onChange={this.handleEmail} value={this.state.email} type="email" placeholder="Email" required />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required />
+          <input onChange={this.handlePassword} value={this.state.password} type="password" placeholder="Password" required />
         </div>
         <div className="field">
-          <button>Log In</button>
+          <button onClick={this.handleFormSubmit}>Log In</button>
         </div>
       </form>
     );
